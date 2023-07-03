8eaf9010f145c2a59d4a0a45bb90bd67cbf710a6
Alfredo Aguiar Arce <67090401+alfredoaguiararce@users.noreply.github.com>
2023-07-02 23:29:42 -0700

    Update decorator.js

--- structural-patterns/decorator.js
+++ structural-patterns/decorator.js
@@ -1,10 +1,22 @@
+/**
+ * The code defines a class called Mackbook with a method that returns a price, and a function called
+ * memmory that modifies the Mackbook object to increase its price by 100.
+ * @returns The first console.log statement will return 1000, which is the original price of the
+ * Mackbook.
+ */
+/* The Mackbook class has a method called precio() that returns the price of a Mackbook, which is 1000. */
 class Mackbook{
     precio(){
         return 1000;
     }
 };
 
-const memmory = mac => {
+/**
+ * The function `memmory` takes a `mac` object and modifies its `precio` method to add 100 to the
+ * original price.
+ * @returns The code snippet does not explicitly return anything.
+ */
+const memmory = (mac) => {
     const mackbook = mac.precio();
     mac.precio = function () {
         return mackbook + 100;
